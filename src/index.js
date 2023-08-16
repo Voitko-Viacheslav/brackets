a4c876d7d93c8749661b45ca408fcb786bf9951a
Voitko Viacheslav Ivanovich <voitko-viacheslav-ivanovich1992@ukr.net>
2023-08-16 09:07:10 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  let strArray = [];
+  let k = 0;
+  while (k < str.length) {
+    strArray.push(str[k])
+    k++;
+  }
+
+  for (let i = 0; i < strArray.length; i++) {
+    for (let k = 0; k < bracketsConfig.length; k++) {
+      if (strArray[i] == bracketsConfig[k][0] && strArray[i + 1] == bracketsConfig[k][1]) {
+        strArray.splice(i, 2);
+        i = -1;
+      }
+    }
+  }
+
+  if (strArray.length == 0) {
+    return true;
+  } else {
+    return false;
+  }
+
 }
+
